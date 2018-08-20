bb967cfdc1d00c39834b938ee78f397c3ffa9bba
longnguyen <longnguyenctt@gmail.com>
2018-08-20 11:38:56 +0700

    #1 Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils/analytics.service';
+import { NbThemeService } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
+
+  constructor(
+    private analytics: AnalyticsService,
+    private themeService: NbThemeService
+  ) {
+  }
+
+  ngOnInit() {
+    this.themeService.changeTheme('corporate');
+    this.analytics.trackPageViews();
+  }
+}
